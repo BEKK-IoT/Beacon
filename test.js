e534d3c5f13de6ec471925573430174321b6d261
Birgitte Haavardsholm <birgittehaavardsholm@mac-birgittehaav.local>
2016-09-15 21:25:53 +0200

    laget et utgangspunkt, rom for refaktorering

--- test.js
+++ test.js
@@ -6,5 +6,9 @@ Beacon.on('discover', function(Beacon) {
   console.log('Beacon found: ' + JSON.stringify(Beacon));
 });
 
+Beacon.on('telemetrydata', function(telemetrydata){
+	console.log('telemetrydata: ', telemetrydata);
+});
+
 Beacon.startScanning(/*'e2c56db5dffb48d2b060d0f5a71096e0', 0, 0*/);
 
