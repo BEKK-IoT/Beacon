9f691afef0c8fe9a6515c301c52f74b2b3452ace
Snorre Lothar von Gohren Edwin <snorre.edwin@gmail.com>
2016-10-03 19:15:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ beacon.BleuStation = require('./bleu-station/bleu-station');
 beacon.Estimote = require('./estimote/estimote');
 beacon.EstimoteSticker = require('./estimote-sticker/estimote-sticker');
 
-process.on('SIGINT', beacon.stopScanning);
+process.on('SIGINT', function() {
+  beacon.stopScanning();
+  beacon.stopAdvertising();
+});
 
 module.exports = {
   beacon: beacon
